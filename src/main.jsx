96b3e6d4660b6d575cb2784e43c501ccad23aab1
ZiadMostafa01 <ziadmostafa9866@gmail.com>
2026-02-25 20:22:47 +0200

    Update

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/vet-clinic/">
+    <BrowserRouter basename="/karas-project/">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
